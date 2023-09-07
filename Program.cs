19e57054f70b4529d833488410015e7a90af2d85
Vyacheslav Minaev <slavik.minaeff@yandex.ru>
2023-09-07 12:01:40 +0300

    Решил вторую задачу

--- Program.cs
+++ Program.cs
@@ -18,3 +18,44 @@
 //     }
 //     return (start + " " + PrintNumbers(start - 1 , end));
 // }
+
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+// System.Console.WriteLine("Введите число M : ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+
+// System.Console.WriteLine("Введите число N : ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// int SumEl(int num2, int num1)
+// {
+//     if (num2 == num1) return num2;
+//     else return SumEl(num2 + 1, num1) + num2;
+// }
+
+// System.Console.WriteLine(SumEl(num1, num2));
+
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+System.Console.WriteLine("Введите число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 <0 || num2 <0)
+{
+    System.Console.WriteLine("Ошибка ввода");
+}
+
+int AkkerFun(int num2, int num1)
+{
+    if (num2 == num1) return num2;
+    else return SumEl(num2 + 1, num1) + num2;
+}
